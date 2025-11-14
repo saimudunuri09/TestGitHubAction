39882fa73555357eb41499ca4cb8866553cf3ffd
saimudunuri999@gmail.com <mudunurisai999@gmail.com>
2025-11-14 13:17:15 -0500

    modified the error

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import app
 def test_home():
     response=app.test_client().get("/")
     assert response.status_code==200
-    assert response.data ==b" Hello World"
+    assert response.data ==b"Hello World"
